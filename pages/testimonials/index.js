5642d386bc0ffff2e4edeed601f1cd11be19362b
Moeed Rizwan <142339044+rexmoeed@users.noreply.github.com>
2025-05-05 16:22:44 +0500

    Add files via upload

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,43 +1,50 @@
-
-
-//components
-import TestimonialSliderSlider from '../../components/TestimonialSlider';
+// components
 import Bulb from '../../components/Bulb';
 import Circles from '../../components/Circles';
 
-//framer motion 
+// framer motion 
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 import TestimonialSlider from '../../components/TestimonialSlider';
 
 const Testimonials = () => {
   return (
-    <div className='h-full bg-primary/30 py-36 flex items-center'>
+    <div className='min-h-screen bg-primary/30 py-36 flex items-center relative z-10'>
       <Circles />
       <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8 '>
+        <div className='flex flex-col xl:flex-row gap-x-8 items-center xl:items-start'>
           {/* text */}
-          <div className='text-center flex xl:w-[300vw] flex-col lg:text-left mb-4 xl:mb-0 '>
-            <motion.h2 variants={fadeIn('up', 0.2)} 
-            initial="hidden" 
-            animate="show" 
-            exit="hidden" 
-            className='h2 text-[28px] sm:text-[32px] md:text-[36px] xl:mt-5'>Hear What My Clients Say<span className='text-accent'>!</span></motion.h2>
-            <motion.p variants={fadeIn('up', 0.4)} 
-            initial="hidden" 
-            animate="show" 
-            exit="hidden" className='mb-4 max-w-[300px] mx-auto lg:mx-0'> I’m grateful for amazing clients and their kind feedback.
+          <div className='text-center flex flex-col lg:text-left mb-8 xl:mb-0 xl:max-w-xl'>
+            <motion.h2
+              variants={fadeIn('up', 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className='h2 xl:mt-5'
+            >
+              Hear What My Clients Say<span className='text-accent'>!</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className='mb-4 max-w-[300px] mx-auto lg:mx-0'
+            >
+              I’m grateful for amazing clients and their kind feedback.
             </motion.p>
           </div>
+
           {/* slider */}
-          <motion.div variants={fadeIn('down', 0.6)} 
-            initial="hidden" 
-            animate="show" 
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
             exit="hidden"
-            className='w-full xl:max-w-[60%]'>
-                <TestimonialSlider />
+            className='w-full xl:max-w-[60%]'
+          >
+            <TestimonialSlider />
           </motion.div>
-          
         </div>
       </div>
       <Bulb />

e6aeee42cc485a556eefd96cb08f987cb9389472
Moeed Rizwan <142339044+rexmoeed@users.noreply.github.com>
2025-05-05 15:13:44 +0500

    Add files via upload

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -54,7 +54,7 @@ const serviceData = [
 
 const ServiceSlider = () => {
   return (
-    <div className="w-full max-w-[1200px] mx-auto px-4">
+    <div className="w-full max-w-[900px] mx-auto px-4">
       <Swiper
         breakpoints={{
           320: {
